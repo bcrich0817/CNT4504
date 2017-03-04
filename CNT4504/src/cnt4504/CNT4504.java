059c547db346170a63b6cba201d6f0894b834523
bcrich0817 <bcrich0817@gmail.com>
2017-03-04 18:35:52 -0500

    Add files via upload

---
+++ CNT4504/src/cnt4504/CNT4504.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cnt4504;
+
+/**
+ *
+ * @author bcrich0817
+ */
+public class CNT4504 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Current Server Date & Time:"); // Display the string.
+        System.out.println("Server Number of Running Processes:"); // Display the string.
+        System.out.println("Server Number of Active Socket Connections:"); // Display the string.
+        System.out.println("Server Time of Last System Boot:"); // Display the string.
+        System.out.println("Server Current Users :"); // Display the string.
+        System.out.println("Server Echo Back What is Sent From Client:"); // Display the string.
+        System.out.println("Quit:"); // Display the string.
+                     
+    }
+    
+}
